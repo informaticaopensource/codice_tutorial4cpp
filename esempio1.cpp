8095897d19cd544cda91fba1372f662e5ce93b0d
informaticaopensource <91938620+informaticaopensource@users.noreply.github.com>
2021-10-09 18:38:25 +0200

    Update esempio1.cpp
    Codice di esempi di sotto programmi che richiedonjo il passaggio di parametri per indirizzo o per valore

--- esempio1.cpp
+++ esempio1.cpp
@@ -1,9 +1,10 @@
+/* Codice di esempi di sotto programmi che richiedonjo il passaggio di parametri per indirizzo o per valore */
 #include <iostream>
 using namespace std;
 void somma(int a,int b, int& s)
 {
 	s=a+b;
-	cout << "La somma è: "<< s << endl;
+	cout << "La somma Ã¨: "<< s << endl;
 }
 int main ()
 {
@@ -12,6 +13,6 @@ int main ()
 	cout << "\nInserisci i due numeri \t";
 	cin >> n1 >> n2;
 	somma(n1,n2,tot);
-	cout << "\n La somma è: \t" << tot << endl;
+	cout << "\n La somma Ã¨: \t" << tot << endl;
 	return 0;
 }
